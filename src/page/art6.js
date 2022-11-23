c4d9a04185be04e3aa9442ce5ae036bdcc34cfa0
narr0225 <nattakornn193@gmail.com>
2022-11-23 16:57:07 +0700

    first commit

--- src/page/art6.js
+++ src/page/art6.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import vide1 from '../video/art6V.mp4'
-<<<<<<< HEAD
-=======
 import poster from '../video/poster/posYoutube3.jpg'
->>>>>>> 577173f (first commit)
 
 function Art6() {
   return (
@@ -12,11 +9,7 @@ function Art6() {
           <h1>MR</h1>
           <h2>MIX REALITY</h2>
           {/* <img id='vrg' src={vrg} alt="vrg"/> */}
-<<<<<<< HEAD
-          <video src={vide1} controls />
-=======
           <video src={vide1} poster={poster} controls />
->>>>>>> 577173f (first commit)
           <p>In the virtual world (MR Mode), there is a description tag that informs the relevant information in each room in which the user has to wear the VR glasses. The user can change the language by adjusting the camera angle to the center of the white color to change the national flag button and there will be a timer wheel to confirm to change the language.</p>
         </div>
     </div>
